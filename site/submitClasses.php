c23d2d3b58f29298a8debbd011515ba3d96ee015
mlanden <mlanden@umbc.edu>
2016-03-21 18:41:05 -0400

    started final script

--- site/submitClasses.php
+++ site/submitClasses.php
@@ -1,3 +1,17 @@
 <?php
-var_dump($_POST);
+
+$studentId = $_POST['campusId'];
+$classes = $_POST['submitclass'];
+$classList = explode(",", $classes);
+
+$dbc = mysql_connect("studentdb-maria.gl.umbc.edu", "dale2", "cmsc433") or die(mysql_error());
+mysql_select_db("dale2", $dbc);
+
+foreach($classList as $class){
+	$inx = strpos($class, ':');
+	$key = substr($class, 0, $inx);
+	$sql = "INSERT INTO `StudentCourses`(`courseID`, `studentID`) VALUES ('$key','$studentId')";
+	echo $key;
+	mysql_query($sql, $dbc);
+}
 ?>
